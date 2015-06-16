e70dfa5b6b09661428743face1b53231940edae8
Andrew <andrewfribush@gmail.com>
2015-06-15 22:06:01 -0400

    Last check and beautification

--- codeigniter/application/views/welcome_message.php
+++ codeigniter/application/views/welcome_message.php
@@ -1,57 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <meta name="author" content="Andrew Fribush">
 
-		<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	</head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Andrew Fribush">
 
-	<body ng-app="studentApp" ng-controller="studentCtrl">
-		<div class="container">
-		
-			<nav class="navbar navbar-default">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<a class="navbar-brand">CISeed Project</a>
-					</div>
-				</div>
-			</nav>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Username</th>
-						<th>Password</th>
-						<th>Randomize</th>
-					</tr>
-				</thead>
-				<tbody>
-                <tr ng-repeat='student in studentData'>
-                   <td> {{student.user_name}}  </td>   
-                   <td> {{student.password}}  </td>
-                   <td> <input class="btn btn-failure" type="submit" id="submit" value="Randomize" ng-click="update(student.id)"></td>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+
+<body ng-app="studentApp" ng-controller="studentCtrl">
+    <div class="container">
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand">CISeed Project</a>
+                </div>
+            </div>
+        </nav>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>Randomize</th>
                 </tr>
-            	</tbody>
-            </table>
-			<div class="row-fluid">
-				<div class="col-md-13">
-				<legend>Create Random User</legend>
+            </thead>
+            <tbody>
+                <tr ng-repeat='student in studentData'>
+                    <td> {{student.user_name}} </td>
+                    <td> {{student.password}} </td>
+                    <td>
+                        <input class="btn btn-failure" type="submit" id="submit" value="Randomize" ng-click="update(student.id)">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="row-fluid">
+            <div class="col-md-13">
+                <legend>Create Random User</legend>
 
-                <form ng-submit="create()"  class="form-inline">
-                	<input class='btn btn-success'type="submit" id="submit" value="Create"/>
+                <form ng-submit="create()" class="form-inline">
+                    <input class='btn btn-success' type="submit" id="submit" value="Create" />
                 </form>
 
             </div>
         </div>
-	</div>
-			
-	</body>
+    </div>
 
-		<script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.js"></script>
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script>
-		<script src="/ciseed/codeigniter/js/controllers.js"></script>
+</body>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.js"></script>
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script>
+<script src="/ciseed/codeigniter/js/controllers.js"></script>
 
 </html>
--- codeigniter/js/controllers.js
+++ codeigniter/js/controllers.js
@@ -1,51 +1,53 @@
 var userList = angular.module('studentApp', []);
 var studentData = [];
 
-userList.controller('studentCtrl', function($scope,$http){
-  var dataLoad = function(){
-  var getData = $http.get("/ciseed/codeigniter/welcome/get");
+userList.controller('studentCtrl', function($scope, $http) {
+    var dataLoad = function() {
+        var getData = $http.get("/ciseed/codeigniter/welcome/get");
 
-  getData.success(function(data,status,headers,config){
-    data = data.split('</div>');
-    data = angular.fromJson(data[1])
-    $scope.studentData = data;
-  });
+        getData.success(function(data, status, headers, config) {
+            data = data.split('</div>');
+            data = angular.fromJson(data[1])
+            $scope.studentData = data;
+        });
 
-  getData.error(function(data, status, headers, config){
-    console.log("Error in data loading");
-  });
-};
+        getData.error(function(data, status, headers, config) {
+            console.log("Error in data loading");
+        });
+    };
 
-dataLoad();
+    dataLoad();
 
-  $scope.create = function(){
-    var create = $http.post('/ciseed/codeigniter/welcome/create');
+    $scope.create = function() {
+        var create = $http.post('/ciseed/codeigniter/welcome/create');
 
-    create.success(function(data,status,headers,config){
-      if(status == 200){
-        dataLoad();
-      } else{
-        console.log(data);
-        console.log(status);
-      }
-    });
-};
+        create.success(function(data, status, headers, config) {
+            if (status == 200) {
+                dataLoad();
+            } else {
+                console.log(data);
+                console.log(status);
+            }
+        });
+    };
 
-  $scope.update = function(id){
-    var update = $http({
-      method: 'POST',
-      url: 'welcome/random_update',
-      data: id,
-      headers: {'Content-Type': 'application/json; charset=UTF-8'}
-    });
+    $scope.update = function(id) {
+        var update = $http({
+            method: 'POST',
+            url: 'welcome/random_update',
+            data: id,
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        });
 
-    update.success(function(data,status,headers,config){
-      if(status == 200){
-        dataLoad();
-      }else{
-        console.log(data);
-        console.log(status);
-      }
-    });
-  }
+        update.success(function(data, status, headers, config) {
+            if (status == 200) {
+                dataLoad();
+            } else {
+                console.log(data);
+                console.log(status);
+            }
+        });
+    }
 });
