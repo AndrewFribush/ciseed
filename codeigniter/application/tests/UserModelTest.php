d4920519a112a72b3f2d316363ddb9a3b7b48ba0
Andrew <andrewfribush@gmail.com>
2015-06-15 21:27:26 -0400

    Tests working

--- codeigniter/application/tests/UserModelTest.php
+++ codeigniter/application/tests/UserModelTest.php
@@ -10,15 +10,30 @@ class UserModelTest extends CIUnit_Framework_TestCase
     }
 
     public function testInsertStudent(){
-
+        $this->studentid = $this->user->insert_student($user_name = 'Andrew44', $password = 'mathy');
+        $this->assertGreaterThan(0, $this->studentid, 'Insert_Student not functiong (in user_model.php, lines 11-22)')
     }
 
     public function testGetStudents(){
-
+        $this->testInsertStudent();
+        $this->assertTrue(count($this->user->get_students($offset = 0, $limit = 20)) >= 1, 'Get not functioning (in user_model.php, lines 24-31)');
     }
 
     public function testUpdateStudentById(){
-
+        $this->testInsertStudent();
+        $this->user->update_student_by_id($this->studentid, $user_name = "Michael44", $password = "englishy");
+        $student_grouped = $this->user->get_students($offset=0, $limit = 100);
+        $student_count = count($this->user->get_students($offset = 0, $limit = 100));
+        if($student_count > 0){
+            foreach($student_grouped as $student){
+                if($student['id]'] == $this->studentid){
+                    $this->assertEquals('Michael44', $student['user_name'], 'username not updated (in user_model.php, lines 34-9');
+                    $this->assertEquals('englishy', $student['password'], 'password not updated (in user_model.php, lines 34-9');
+                }
+            }
+        } else{
+            echo "Zero students returned. This might be a problem with InsertStudent";
+        }
     }
 
     protected function clean(){
--- codeigniter/application/tests/WelcomeControllerTest.php
+++ codeigniter/application/tests/WelcomeControllerTest.php
@@ -9,19 +9,35 @@ class WelcomeControllerTest extends CIUnit_Framework_TestCase
     }
 
     public function testGet(){
-
+        $creation = $this->get_instance()->create();
+        $this->assertTrue($creation !== NULL, 'The get is not returning (in welcome.php, 9-17');
+        $this->assertTrue(json_decode($creation) !== NULL, 'JSON coding not working (in welcome.php check traversal and lines 11-3.')
     }
 
     public function testCreate(){
-
+        $creation = $this->get_instance()->create();
+        $this->studentid = json_decode($creation)->id;
+        $this->assertTrue($createdStudent !==0, 'Create is not functioning (in welcome.php, 19-33');
     }
 
     public function testRandomUpdate(){
+        $creation = $this->get_instance()->create();
+        $creationObj = json_decode($createdStudent);
+        $this->studentid= $creationObj->id;
 
+        $idsaver = $this->studentid;
+        $updatedCreation = $this->get_instance()->update_student();
+        $updatedCreationObj = json_decode($updatedStudent);
+
+        $this->assertTrue($updatedCreation !== 0, 'No returned object, (in welcome.php, probably 35-6 or 42)');
+        $this->assertTrue($updatedCreationObj->user_name !== $creationObj->user_name, 'Student username not updated (in welcome.php, 35-42). Note, there is a very small chance of this giving a false failure due to random generation allowing the possibility of repitition.');
+        $this->assertTrue($updatedCreationObj->password !== $creationObj->password, 'Student password not updated (in welcome.php, 35-42). Note, there is a very small chance of this giving a false failure.')
     }
 
     protected function clean(){
-
+        $idsaver = $this->studentid;
+        $this->db->where('id', $idsaver);
+        $this->db->delete('Student', $data);
     }
 
 }
