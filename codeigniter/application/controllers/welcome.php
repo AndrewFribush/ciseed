602a3aff4b9ac2da97100d0b5255837708e2801b
Andrew <andrewfribush@gmail.com>
2015-06-15 20:49:06 -0400

    API mostly working

--- codeigniter/application/controllers/welcome.php
+++ codeigniter/application/controllers/welcome.php
@@ -7,15 +7,46 @@ class Welcome extends CI_Controller {
 	}
 
 	public function get(){
-
+		$students = $this->user_model->get_students();
+		$data['students'] = $students;
+		if($data['students'] !== FALSE){
+			echo json_encode($data['students']);
+		} else{
+			echo json_encode("No data returned");
+		}
 	}
 	
 	public function create(){
+		$this->load->helper('string');
+		$username = random_string('alpha', rand(3,10));
+		$password = random_string('alnum', rand(3,10));
 
+		$creation = $this->user_model->insert_student($username, $password);
+
+		if($creation !== FALSE){
+			$new_student = array('id' => NULL, 'username' => $username, 'password' => $password);
+			echo json_encode($new_student);
+		} else{
+			echo json_encode('Creation failed');
+		}
 	}
 
 	public function random_update(){
+		if($id){
+			$this->load->helper('string');
+			$username = random_string('alpha', rand(3,10));
+			$password = random_string('alnum', rand(3,10));
 
+			$update = $this->user_model->update_student_by_id($id, $username, $password);
+
+			if($update !== FALSE){
+				$updated_student = array('username' => $username, 'password' => $password);
+			} else{
+				echo json_encode('Update failed to push up');
+			}
+		} else{
+			echo json_encode('Missing id');
+		}
 	}
 
 }
--- codeigniter/application/models/user_model.php
+++ codeigniter/application/models/user_model.php
@@ -9,15 +9,39 @@ class User_model extends CI_Model
     }
     
     public function insert_student($username, $password){
-
+        $data = array(
+            'user_name' => $username,
+            'password' => $password,
+            );
+        $this->db->insert('Student', $data);
+        if($this->db->insert_id()){
+            return $this->db->insert_id();
+        } else{
+            return FALSE;
+        }
     }
 
     public function get_students($offset = 0, $limit = 20){
-
+        $query = $this->db->get('Student', $limit, $offset);
+        if($query->num_rows() > 0){
+            return $query->result_array();
+        } else{
+            return FALSE;
+        }
     }
 
     public function update_student_by_id($id, $username, $password){
-
+        $data = array(
+            'user_name' => $username,
+            'password' => $password,
+            );
+        $this->db->where('id', $id);
+        $this->db->update('Student', $data);
+        if($this->db->affected_rows() >0){
+            return TRUE;
+        } else{
+            return FALSE;
+        }
     }
 }
 ?>
